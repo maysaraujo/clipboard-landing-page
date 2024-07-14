901861b0ab58c7b6c0aee0514d8a2f5fb80e3ee0
maysaraujo <maysaaraujo25@gmail.com>
2024-07-14 11:50:25 -0300

    feat: adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import ClipboardSection from './components/ClipboardSection';
+import FirstSection from './components/FirstSection';
+import Footer from './components/Footer';
 import Header from './components/Header';
+import Partners from './components/Partners';
+import SecondSection from './components/SecondSection';
+import ThirdSection from './components/ThirdSection';
 
 function App() {
   return (
     <>
       <Header />
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
+      <Partners />
+      <ClipboardSection />
+      <Footer />
     </>
   );
 }
