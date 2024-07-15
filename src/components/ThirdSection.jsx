201c15d99318d4670331e2f54ba75517d0d3cead
maysaraujo <maysaaraujo25@gmail.com>
2024-07-15 12:05:51 -0300

    feat: adding SectionText and Tools component

--- src/components/ThirdSection.jsx
+++ src/components/ThirdSection.jsx
@@ -1,55 +1,38 @@
 import IconBlackList from '../assets/images/icon-blacklist.svg';
 import IconText from '../assets/images/icon-text.svg';
 import IconPreview from '../assets/images/icon-preview.svg';
+import SectionText from './SectionText';
+import Tools from './Tools';
 
 const ThirdSection = () => {
   return (
     <section className='bg-white pt-20'>
       <div className='pageCenter'>
-        <h2 className='text-dark-grayish-blue text-center text-3xl font-semibold'>
-          Superchange your workflow
-        </h2>
-        <div className='flex justify-center'>
-          <p className='text-grayish-blue text-center mt-4 mb-16 max-w-[19.375rem]'>
-            We've got the tools to boost your productivity.
-          </p>
-        </div>
+        <SectionText
+          title='Superchange your workflow'
+          text="We've got the tools to boost your productivity."
+        />
 
-        <div className='flex justify-center'>
-          <img src={IconBlackList} alt='Blacklist icon' />
-        </div>
-        <h3 className='text-dark-grayish-blue text-center text-2xl font-semibold mt-8'>
-          Create blacklists
-        </h3>
-        <div className='flex justify-center'>
-          <p className='text-grayish-blue text-center mt-4 mb-12 max-w-[19.375rem]'>
-            Ensure sensitive information never makes its way to your clipboard
-            by excluding certain sources.
-          </p>
-        </div>
-
-        <div className='flex justify-center'>
-          <img src={IconText} alt='Text icon' />
-        </div>
-        <h3 className='text-dark-grayish-blue text-center text-2xl font-semibold mt-8'>
-          Plain text snippets
-        </h3>
-        <div className='flex justify-center'>
-          <p className='text-grayish-blue text-center mt-4 mb-12 max-w-[19.375rem]'>
-            Remove unwanted formatting from copied text for a consistent look.
-          </p>
-        </div>
-
-        <div className='flex justify-center'>
-          <img src={IconPreview} alt='Preview icon' />
-        </div>
-        <h3 className='text-dark-grayish-blue text-center text-2xl font-semibold mt-8'>
-          Sneak preview
-        </h3>
-        <div className='flex justify-center'>
-          <p className='text-grayish-blue text-center mt-4 max-w-[19.375rem]'>
-            Quick preview of all snippets on your Clipboard for easy access.
-          </p>
+        <div className='lg:flex lg:justify-center lg:items-baseline lg:gap-8'>
+          <Tools
+            image={IconBlackList}
+            alt='Blacklist icon'
+            title='Create blacklists'
+            text='Ensure sensitive information never makes its way to your clipboard
+              by excluding certain sources.'
+          />
+          <Tools
+            image={IconText}
+            alt='Text icon'
+            title='Plain text snippets'
+            text='Remove unwanted formatting from copied text for a consistent look.'
+          />
+          <Tools
+            image={IconPreview}
+            alt='Preview icon'
+            title='Sneak preview'
+            text='Quick preview of all snippets on your Clipboard for easy access.'
+          />
         </div>
       </div>
     </section>
