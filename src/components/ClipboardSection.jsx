f611f1116a918722e1bb5bfd4b83f2b1244265c9
maysaraujo <maysaaraujo25@gmail.com>
2024-07-15 12:01:06 -0300

    refactor: adding SectionText component

--- src/components/ClipboardSection.jsx
+++ src/components/ClipboardSection.jsx
@@ -1,18 +1,15 @@
 import Buttons from './Buttons';
+import SectionText from './SectionText';
 
 const ClipboardSection = () => {
   return (
     <section className='bg-white py-20'>
       <div className='pageCenter'>
-        <h2 className='text-dark-grayish-blue text-center text-3xl font-semibold'>
-          Clipboard for iOS and Mac OS
-        </h2>
-        <div className='flex justify-center'>
-          <p className='text-grayish-blue text-center mt-4 mb-12 max-w-[19.375rem]'>
-            Available for free on the App Store. Download for Mac or iOS, sync
-            with iCloud and you're ready to start adding to your clipboard.
-          </p>
-        </div>
+        <SectionText
+          title='Clipboard for iOS and Mac OS'
+          text="Available for free on the App Store. Download for Mac or iOS, sync
+            with iCloud and you're ready to start adding to your clipboard."
+        />
 
         <Buttons />
       </div>
