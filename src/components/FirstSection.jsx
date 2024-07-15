2940af64bdaf1edf054467c9b28dc5993a42597c
maysaraujo <maysaaraujo25@gmail.com>
2024-07-15 12:11:02 -0300

    feat: adding pageCenter class to mobile

--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -5,35 +5,37 @@ import TextFirstSection from './TextFirstSection';
 const FirstSection = () => {
   return (
     <section className='bg-white pt-20'>
-      <SectionText
-        title='Keep track of your snippets'
-        text='Clipboard instantly stores any item you copy in the cloud, meaning you
+      <div className='pageCenter lg:hidden'>
+        <SectionText
+          title='Keep track of your snippets'
+          text='Clipboard instantly stores any item you copy in the cloud, meaning you
           can access your snippets immediately on all your devices. Our Mac and
           iOS apps will help you organize everything.'
-      />
-
-      <div className='lg:flex lg:gap-32'>
-        <img
-          className='lg:ml-[-2rem]'
-          src={ImageComputer}
-          alt='Image Computer'
         />
 
-        <div className=''>
-          <TextFirstSection
-            title='Quick Search'
-            text='Easily search your snippets by content, category, web address,
+        <div className='lg:flex lg:gap-32'>
+          <img
+            className='lg:ml-[-2rem]'
+            src={ImageComputer}
+            alt='Image Computer'
+          />
+
+          <div className=''>
+            <TextFirstSection
+              title='Quick Search'
+              text='Easily search your snippets by content, category, web address,
                 application, and more.'
-          />
-          <TextFirstSection
-            title='iCloud Sync'
-            text='Instantly saves and syncs snippets across all your devices.'
-          />
-          <TextFirstSection
-            title='Complete History'
-            text='Retrieve any snippets from the first moment you started using
+            />
+            <TextFirstSection
+              title='iCloud Sync'
+              text='Instantly saves and syncs snippets across all your devices.'
+            />
+            <TextFirstSection
+              title='Complete History'
+              text='Retrieve any snippets from the first moment you started using
                 the app.'
-          />
+            />
+          </div>
         </div>
       </div>
     </section>
