3fe3b2ae2e789c92a7bca51e8915ea1241c95a5c
maysaraujo <maysaaraujo25@gmail.com>
2024-07-14 22:31:10 -0300

    feat: adding background desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,23 @@
 import Logo from '../assets/images/logo.svg';
+import Buttons from './Buttons';
 
 const Header = () => {
   return (
-    <header className='bg-header-mobile bg-no-repeat bg-right bg-cover md:hidden'>
+    <header className='bg-header-mobile bg-no-repeat bg-[length:100%] lg:bg-header-desktop'>
       <div className='pageCenter'>
-        <div className='flex justify-center pt-[9.375rem] pb-16'>
+        <div className='flex justify-center pt-[7.375rem] pb-16'>
           <img className='' src={Logo} alt='Logo' />
         </div>
         <h1 className='text-dark-grayish-blue text-center text-4xl font-semibold'>
           A history of everything you copy
         </h1>
         <div className='flex justify-center'>
-          <p className='text-grayish-blue text-center mt-4 mb-12 max-w-[19.375rem]'>
+          <p className='text-grayish-blue text-center mt-4 mb-12 max-w-[19.375rem] lg:max-w-[33.75rem]'>
             Clipboard allows you to track and organize everything you copy.
             Instantly access your clipboard on all your devices.
           </p>
         </div>
-        <div className='flex justify-center items-center flex-col gap-5'>
-          <button className='bg-strong-cyan text-white py-2.5 rounded-3xl w-4/5 font-semibold'>
-            Download for iOS
-          </button>
-          <button className='bg-light-blue text-white py-2.5 rounded-3xl w-4/5 font-semibold'>
-            Download for Mac
-          </button>
-        </div>
+        <Buttons />
       </div>
     </header>
   );
