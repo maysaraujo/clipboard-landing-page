d16de2251aa7b38ab04fc106117c80ec3627fde2
maysaraujo <maysaaraujo25@gmail.com>
2024-07-15 12:05:37 -0300

    feat: adding SectionText component

--- src/components/SecondSection.jsx
+++ src/components/SecondSection.jsx
@@ -1,20 +1,19 @@
 import ImageDevices from '../assets/images/image-devices.png';
+import SectionText from './SectionText';
 
 const SecondSection = () => {
   return (
     <section>
       <div className='pageCenter pt-20'>
-        <h2 className='text-dark-grayish-blue text-center text-3xl font-semibold'>
-          Access Clipboard Anywhere
-        </h2>
-        <div className='flex justify-center'>
-          <p className='text-grayish-blue text-center mt-4 mb-12 max-w-[19.375rem]'>
-            Whether you're on the go, or at your computer, you can access all
-            your Clipboard snippets in a few simple clicks.
-          </p>
-        </div>
+        <SectionText
+          title='Access Clipboard Anywhere'
+          text="Whether you're on the go, or at your computer, you can access all
+            your Clipboard snippets in a few simple clicks."
+        />
 
-        <img src={ImageDevices} alt='Image devices' />
+        <div className='flex justify-center'>
+          <img src={ImageDevices} alt='Image Devices' />
+        </div>
       </div>
     </section>
   );
