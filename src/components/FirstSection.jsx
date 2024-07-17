e72a0229b9defeb8b0593f073bd7d46bac9c033b
maysaraujo <maysaaraujo25@gmail.com>
2024-07-16 22:22:33 -0300

    feat: adding desktop section

--- src/components/FirstSection.jsx
+++ src/components/FirstSection.jsx
@@ -38,6 +38,40 @@ const FirstSection = () => {
           </div>
         </div>
       </div>
+
+      <div className='sm:hidden lg:block'>
+        <SectionText
+          title='Keep track of your snippets'
+          text='Clipboard instantly stores any item you copy in the cloud, meaning you
+          can access your snippets immediately on all your devices. Our Mac and
+          iOS apps will help you organize everything.'
+        />
+
+        <div className='lg:flex lg:gap-32'>
+          <img
+            className='lg:ml-[-2rem]'
+            src={ImageComputer}
+            alt='Image Computer'
+          />
+
+          <div className=''>
+            <TextFirstSection
+              title='Quick Search'
+              text='Easily search your snippets by content, category, web address,
+                application, and more.'
+            />
+            <TextFirstSection
+              title='iCloud Sync'
+              text='Instantly saves and syncs snippets across all your devices.'
+            />
+            <TextFirstSection
+              title='Complete History'
+              text='Retrieve any snippets from the first moment you started using
+                the app.'
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
